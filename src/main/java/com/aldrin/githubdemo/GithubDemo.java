52c6861ab9c8a8aca2396097617693b3a78663f1
@ldrin <Lenovo@DESKTOP-DA8S34H>
2022-07-14 21:54:43 +0800

    Github Demo Project

--- src/main/java/com/aldrin/githubdemo/GithubDemo.java
+++ src/main/java/com/aldrin/githubdemo/GithubDemo.java
@@ -12,6 +12,6 @@ package com.aldrin.githubdemo;
 public class GithubDemo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        System.out.println("Welcome to Java programming with aldrin");
     }
 }
